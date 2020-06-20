232758c629c47b4b7a87bcbe9ff8cd2d02588268
MohammedBaqer-544 <almousawi2002@gmail.com>
2020-06-20 20:27:25 +0300

    تم

---
+++ ios-cw3-p1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var number1 = 3
+var number2 = 7
+
+func add( a: Int, b: Int ) -> Int
+{
+   return a + b
+}
+
+var functionCall = add(a: number1, b: number2)
+
+print("\(number1) + \(number2) = \(functionCall)")
+
+print(functionCall)
